1bc51e4ae6e2490f117deff7f2180b90dc0c9779
charuK1002 <charukhandelwal610@gmail.com>
2026-03-17 14:40:54 +0530

    adding clean_text function to ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -1,1 +1,8 @@
-.
+import re
+import pdfplumber
+from vector_store import add_document
+
+def clean_text(text):
+    text = re.sub(r'\(cid:\d+\)', '', text)
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
