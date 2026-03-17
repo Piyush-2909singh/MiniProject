54e705b9aff8ac1025aee5099276c330b56c74b8
charuK1002 <charukhandelwal610@gmail.com>
2026-03-17 15:07:31 +0530

    add index and metadata loading logic

--- search.py
+++ search.py
@@ -7,3 +7,14 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 
 NDEX_PATH = "vector_db/index.faiss"
 META_PATH = "vector_db/metadata.pkl"
+
+if os.path.exists(INDEX_PATH):
+    index = faiss.read_index(INDEX_PATH)
+else:
+    index = None
+
+if os.path.exists(META_PATH):
+    metadata = pickle.load(open(META_PATH,"rb"))
+else:
+    metadata = []
+    
