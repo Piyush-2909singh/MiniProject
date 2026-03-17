f49f267342d3d2fa3c222bf9b52ec7de6a0a6723
charuK1002 <charukhandelwal610@gmail.com>
2026-03-17 15:23:18 +0530

    configure embedding model and FAISS index/metadata paths

--- vector_store.py
+++ vector_store.py
@@ -3,3 +3,7 @@ import pickle
 import os
 from sentence_transformers import SentenceTransformer
 
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+INDEX_PATH = "vector_db/index.faiss"
+META_PATH = "vector_db/metadata.pkl"
