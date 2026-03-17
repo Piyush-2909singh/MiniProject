f6d00c822c063c613dfeeae89d27651eb30520c0
charuK1002 <charukhandelwal610@gmail.com>
2026-03-17 15:03:12 +0530

    configure storage paths for FAISS index and metadata files

--- search.py
+++ search.py
@@ -4,3 +4,6 @@ import os
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
+
+NDEX_PATH = "vector_db/index.faiss"
+META_PATH = "vector_db/metadata.pkl"
