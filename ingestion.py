84f30264a4c3844c24fdfebd469d76c323a2d820
charuK1002 <charukhandelwal610@gmail.com>
2026-03-17 14:46:48 +0530

    adding ingest_document function to ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -6,3 +6,16 @@ def clean_text(text):
     text = re.sub(r'\(cid:\d+\)', '', text)
     text = re.sub(r'\s+', ' ', text)
     return text.strip()
+
+def ingest_document(path):
+
+    text = ""
+
+    with pdfplumber.open(path) as pdf:
+
+        for page in pdf.pages:
+            t = page.extract_text()
+
+        if t:
+            t = clean_text(t)
+            text += t + "\n"
