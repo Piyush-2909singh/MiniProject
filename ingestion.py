1a77765513ff5f12782c4846819b2dbe02a4b984
charuK1002 <charukhandelwal610@gmail.com>
2026-04-20 01:43:56 +0530

    updating folders

--- ingestion.py
+++ ingestion.py
@@ -20,8 +20,7 @@ def ingest_document(path):
             t = clean_text(t)
             text += t + "\n"
 
-
-
+    # better chunking
     chunk_size = 300
 
     for i in range(0, len(text), chunk_size):
